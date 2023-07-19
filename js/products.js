0041294410a3f5b24023a138bfa8ccc215edc1d3
MinhMVT <129242261+MinhMVT@users.noreply.github.com>
2023-07-19 08:03:36 +0700

    A

--- js/products.js
+++ js/products.js
@@ -2,66 +2,67 @@
 const products = [
     {
         name: 'Garden Gloves',
-        link: '/products/garden-gloves.html',
-        image: 'image/products/garden-gloves.png',
-        description: 'Product Description',
-        price: 9.99
+        link: 'products/garden-product1 1.html',
+        image: 'products/gangtay.jpg',
+        description: 'Garden gloves',
+        price: '$2.99'
     },
     {
         name: 'Pruning Shears',
-        link: '/products/pruning-shears.html.html',
-        image: 'image/products/pruning-shears.png',
-        description: 'Product Description',
-        price: 14.99
+        link: 'products/garden-product1 2.html',
+        image: 'products/keo.jpg',
+        description: 'Garden leaf pruning scissors',
+        price: '$5.99'
     },
     {
         name: 'Loppers',
-        link: '/products/loppers.html',
-        image: 'image/products/loppers.jpg',
-        description: 'Product Description',
-        price: 11.99
+        link: 'products/garden-product1 3.html',
+        image: 'products/xeng.jpg',
+        description: 'Garden shovel',
+        price: '$4.99'
     },
     {
         name: 'Gardening Fork',
-        link: '/products/garden-fork.html',
-        image: 'image/products/garden-fork.jpg',
-        description: 'Product Description',
-        price: 16.99
+        link: 'products/garden-product1 4.html',
+        image: 'products/voinuoc.jpg',
+        description: 'Multi-function sprinkler',
+        price: '$8.99'
     },
     {
         name: 'Hand Trowel',
-        link: '/products/hand-trowel.html',
-        image: 'image/products/hand-trowel.jpg',
-        description: 'Product Description',
-        price: 19.99
+        link: 'products/garden-product1 5.html',
+        image: 'products/chautreo.jpg',
+        description: ' Balcony planter',
+        price: '$4.99'
     },
     {
         name: 'Spade',
-        link: '/products/spade.html',
-        image: 'image/products/spade.jpg',
-        description: 'Product Description',
-        price: 16.99
+        link: 'products/garden-product1 6.html',
+        image: 'products/phanbon.jpg',
+        description: 'Fertilizer for houseplants',
+        price: '$3.99'
     },
     {
         name: 'Rake',
-        link: '/products/rake.html',
-        image: 'image/products/rake.png',
-        description: 'Product Description',
-        price: 18.99
+        link: 'products/garden-product1 7.html',
+        image: 'products/datt.jpg',
+        description: 'Organic soil for houseplants',
+        price: '$6.99'
     },
     {
         name: 'Hoe',
-        link: '/products/hoe.html',
-        image: 'image/products/hoe.png',
-        description: 'Product Description',
-        price: 10.99
+        link: 'products/garden-product1 8.html',
+        image: 'products/trangtri.jpg',
+        description: 'Bonsai shelf',
+        price: '$10.99'
     },
     {
-        name: 'Garden Hose',
-        link: '/products/garden-hose.html',
-        image: 'image/products/garden-hose.png',
-        description: 'Product Description',
-        price: 14.99
+        name: 'SHOW MORE',
+        link: 'G2-CodeMavericks-main/Project My Id/product.html',
+        image:'products/muiten.png',
+        description: 'SHOW MORE',
+        price:'',
     }
+   
   ];
   
